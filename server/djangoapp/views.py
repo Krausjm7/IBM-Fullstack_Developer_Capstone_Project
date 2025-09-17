a05a5590262ea9a7e6477b48ccd402ef3b118f8a
krausjm7 <krausjm7@gmail.com>
2025-09-17 19:13:21 -0400

    Added Dockerfile and entrypoint.sh for containerization and Deployment on Kubernetes pods

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -94,10 +94,19 @@ def get_dealer_reviews(request, dealer_id):
     if dealer_id:
         endpoint = "/fetchReviews/dealer/" + str(dealer_id)
         reviews = get_request(endpoint)
-        for review_detail in reviews:
-            response = analyze_review_sentiments(review_detail['review'])
-            review_detail['sentiment'] = response['sentiment']
-        return JsonResponse({"status": 200, "reviews": reviews})
+
+        # Check if reviews are not None and is a list
+        if reviews is not None and isinstance(reviews, list):
+            for review_detail in reviews:
+                response = analyze_review_sentiments(review_detail['review'])
+                # Check if the response from the sentiment analyzer is valid
+                if response and 'sentiment' in response:
+                    review_detail['sentiment'] = response['sentiment']
+                else:
+                    review_detail['sentiment'] = "unclassified" # Default value
+            return JsonResponse({"status": 200, "reviews": reviews})
+        else:
+            return JsonResponse({"status": 500, "message": "Backend reviews not found."})
     else:
         return JsonResponse({"status": 400, "message": "Bad Request"})
 
